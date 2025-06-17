5532a3c0e42e26b59ed4a8b82bf4253c989ff4f4
Mr Doomsday <randomiki1111@gmail.com>
2025-06-17 14:38:04 +0700

    Update data.php

--- data.php
+++ data.php
@@ -1,10 +1,11 @@
 <?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "user"; // nama database kamu
+$hostname = "sql102.infinityfree.com";
+$username = "if0_39242257";
+$password = "pXXQHVGkQsG2k43";
+$database = "if0_39242257_loginform"; // ganti XXX dengan nama database yang sesuai
+$port = 3306;
 
-$conn = mysqli_connect($hostname, $username, $password, $database);
+$conn = mysqli_connect($hostname, $username, $password, $database, $port);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
